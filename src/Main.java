fd9e268e2b143239eb7b4b66ce2192811085e9aa
LipatoS <sasharubaiko@gmail.com>
2022-12-30 01:23:59 +0200

    ветка develop_1 добавляем метод конвертации "convertMilesToKms"

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Converter for base converting");
-        System.out.println("Version 1.0");
+        System.out.println("Version 1.1");
         double miles = 15;
         double kms = 26;
+        double kmsRes = convertMilesToKms(miles);
+        System.out.println(kmsRes + " kms in " + miles + " miles.");
+    }
+    private static double convertMilesToKms(double miles) {
+        return miles * 1.60934;
     }
 }
